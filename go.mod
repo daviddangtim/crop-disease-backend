c1fc6178beed19560e5444d57ae49f32abe1c78a
daviddangtim <dangtimdavid021@google.com>
2025-03-11 17:15:52 +0100

    Basic file upload

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module crop-disease-backend
 
 go 1.23.4
+
+require github.com/go-chi/chi/v5 v5.2.0 // indirect
--- main.go
+++ main.go
@@ -1,2 +1,63 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+log.Println("File Upload Endpoint Hit")
+
+r.ParseMultipartForm(10 << 20)
+
+file, handler, err := r.FormFile("image")
+if err != nil {
+    log.Println(http.StatusBadRequest)
+    log.Println(err)
+    return
+}
+defer file.Close()
+log.Printf("Uploaded File: %+v\n",handler.Filename)
+log.Printf("File Size: %+v\n",handler.Size)
+log.Printf("MIME Header: %+v\n",handler.Header)
+
+tempFile, err := os.CreateTemp("temp-images", "upload-*.png")
+if err != nil {
+    log.Println(err)
+}
+defer tempFile.Close()
+
+fileBytes, err := io.ReadAll(file)
+if err != nil {
+    log.Println(err)
+}
+
+tempFile.Write(fileBytes)
+
+log.Println(fmt.Fprintf(w, "Successfully Uploaded File\n"))
+
+    
+}
+
+
+
+func startServer()  {
+    mux := http.NewServeMux()
+    mux.HandleFunc("/upload", uploadFile)
+    s := &http.Server{
+        Addr: ":8080",
+        Handler: mux,
+        ReadTimeout: 10 * time.Second,
+        WriteTimeout: 10 * time.Second,
+        MaxHeaderBytes: 1 << 20, //Header size of 1mb
+    }
+    log.Fatal(s.ListenAndServe())    
+}
+
+func main() {
+   startServer()
+}
